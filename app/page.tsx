ea3d0e9f31b43630911c54b3ab685f3cb4c69949
Schmidt Sascha <ss532@hdm-stuttgart.de>
2023-09-16 12:17:11 +0200

    add random categories for HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { fetchNews } from './utils/fetchNews'
+import { categories } from '@/constants/categories'
+import NewsList from './NewsList'
 
-const HomePage = () => {
+const HomePage = async () => {
+
+  const randomCategory = categories[Math.floor(Math.random() * categories.length)]
+  const { articles } = await fetchNews(randomCategory)
   return (
     <div>
-
+      <NewsList articles={articles} />
     </div>
   )
 }
